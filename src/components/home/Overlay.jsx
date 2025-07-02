6ef90038643d5b684db1faf202cfe961aba6a7d9
liuisaac <80558145+liuisaac@users.noreply.github.com>
2025-07-02 00:37:04 -0700

    fixed scaling

--- src/components/home/Overlay.jsx
+++ src/components/home/Overlay.jsx
@@ -8,16 +8,16 @@ const Overlay = () => {
     return (
         <div className="2xl:w-[40em] xl:w-[35em] lg:w-[30em] md:w-[25em] w-full md:px-0 px-10 col items-start justify-center z-30 h-screen">
             <div className="row-center">
-                <Img src="/pfp.svg" alt="pfp" className=" 2xl:w-24 xl:w-16 md:w-16 w-14 aspect-square z-10" />
+                <Img src="/pfp.svg" alt="pfp" className=" 2xl:w-20 xl:w-16 md:w-16 w-14 aspect-square z-10" />
                 <div className="col justify-center items-start ml-4">
-                    <h1 className="2xl:text-5xl xl:text-3xl md:text-3xl text-3xl header z-20">isaac_liu</h1>
-                    <span className="2xl:text-3xl xl:text-xl md:text-xl text-xl text-slate">
+                    <h1 className="2xl:text-4xl xl:text-3xl md:text-3xl text-3xl header z-20">isaac_liu</h1>
+                    <span className="2xl:text-2xl xl:text-xl md:text-xl text-xl text-slate">
                         Vancouver, BC • Software Engineer
                     </span>
                 </div>
             </div>
 
-            <div className="col 2xl:text-2xl xl:text-lg lg:text-md 2xl:gap-8 gap-2 md:my-8 my-16">
+            <div className="col 2xl:text-xl xl:text-lg lg:text-md 2xl:gap-8 gap-2 md:my-8 my-16">
                 <WelcomeMessage />
 
                 <p>i&apos;ll save you some trouble and just cut to the chase:</p>
@@ -26,13 +26,13 @@ const Overlay = () => {
                     <li>• 3rd year computer science student 🆘</li>
                     <li>• Developer @ UBC Biztech 🤖</li>
                     <li>• Engineering @ Mecha Mayhem and Event Control 🚀</li>
-                    <li>• Buidling ChooChoo — a tiny distributed key-value store in Go 🚂</li>
+                    <li>• Buidling ChooChoo — a tiny distributed db in Go 🚂</li>
                     <li>• Former Undergrad TA @ UBC 📖</li>
                     <li>• Actively looking for opportunities in SWE 💯</li>
                 </ul>
 
                 <p>
-                    at my core, im a tinkerer. it&apos;s important to me that i&apos;m constantly taking initiative and ownership of my creation process; to ideate solutions to interesting problems, build them out, and push the limits my comfort zone in the process.
+                    at my core, i&apos;m a tinkerer. i love nothing more than to ideate solutions to interesting problems, build them out, and push the limits my comfort zone in the process.
                 </p>
                 {/* wow look at me i know how to use semicolons */}
             </div>
